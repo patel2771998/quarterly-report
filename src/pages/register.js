384fef404b4b2abb753c1743e7dc5675d2ab63ff
miravpatel <miravthinkinfoservices@gmail.com>
2022-03-09 19:00:40 +0530

    register add

---
+++ src/pages/register.js
@@ -0,0 +1,274 @@
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormHelperText,
+  Link,
+  TextField,
+  Typography
+} from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ApiServices from 'src/config/ApiServices';
+import ApiEndpoint from 'src/config/ApiEndpoint';
+import { toast } from 'react-toastify';
+import { connect } from 'react-redux';
+import { Types } from '../constants/actionTypes';
+const Register = (props) => {
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      name: '',
+      userName: '',
+      password: '',
+      policy: false
+    },
+    validationSchema: Yup.object({
+      email: Yup
+        .string()
+        .email(
+          'Must be a valid email')
+        .max(255)
+        .required(
+          'Email is required'),
+      name: Yup
+        .string()
+        .max(255)
+        .required(
+          'Name is required'),
+      userName: Yup
+        .string()
+        .max(255)
+        .required(
+          'Username is required'),
+      password: Yup
+        .string()
+        .max(255)
+        .required(
+          'Password is required'),
+      policy: Yup
+        .boolean()
+        .oneOf(
+          [true],
+          'This field must be checked'
+        )
+    }),
+    onSubmit: () => {
+      onRegisterPress()
+    }
+  });
+
+  const onRegisterPress = async () => {
+    var body = {
+      'user_name': formik.values.userName,
+      'password': formik.values.password,
+      'name': formik.values.name,
+      'email': formik.values.email
+    }
+    var headers = {
+      "Content-Type": "application/json",
+    }
+    props.loaderRef(true)
+    var data = await ApiServices.PostApiCall(ApiEndpoint.REGISTER_USER, JSON.stringify(body), headers);
+    props.loaderRef(false)
+    console.log(data)
+    if (!!data) {
+      if (data.status) {
+        data.userData.token = data.token
+        props.save_user_data({ user: data.userData });
+        router.push('/');
+        toast.success(data.message)
+      } else {
+        toast.error(data.message)
+      }
+    } else {
+      toast.error('Something went wrong.')
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>
+          Register | TISTrading
+        </title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+          <form onSubmit={formik.handleSubmit}>
+            <Box
+              sx={{
+                justifyContent: 'center',
+                display: 'flex',
+                flexGrow: 1,
+                my: 3
+              }}
+            >
+              <img
+                style={{
+                  height: 100,
+                  width: 100,
+                }}
+                src="/static/images//logo.png" />
+            </Box>
+            <Box sx={{ my: 3 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                Create a new account
+              </Typography>
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                Use your email to create a new account
+              </Typography>
+            </Box>
+            <TextField
+              error={Boolean(formik.touched.name && formik.errors.name)}
+              fullWidth
+              helperText={formik.touched.name && formik.errors.name}
+              label="Name"
+              margin="normal"
+              name="name"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.userName && formik.errors.userName)}
+              fullWidth
+              helperText={formik.touched.userName && formik.errors.userName}
+              label="Username"
+              margin="normal"
+              name="userName"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              value={formik.values.userName}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.email && formik.errors.email)}
+              fullWidth
+              helperText={formik.touched.email && formik.errors.email}
+              label="Email Address"
+              margin="normal"
+              name="email"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="email"
+              value={formik.values.email}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.password && formik.errors.password)}
+              fullWidth
+              helperText={formik.touched.password && formik.errors.password}
+              label="Password"
+              margin="normal"
+              name="password"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="password"
+              value={formik.values.password}
+              variant="outlined"
+            />
+            <Box
+              sx={{
+                alignItems: 'center',
+                display: 'flex',
+                ml: -1
+              }}
+            >
+              <Checkbox
+                checked={formik.values.policy}
+                name="policy"
+                onChange={formik.handleChange}
+              />
+              <Typography
+                color="textSecondary"
+                variant="body2"
+              >
+                I have read the
+                {' '}
+                <NextLink
+                  href="#"
+                  passHref
+                >
+                  <Link
+                    color="primary"
+                    underline="always"
+                    variant="subtitle2"
+                  >
+                    Terms and Conditions
+                  </Link>
+                </NextLink>
+              </Typography>
+            </Box>
+            {Boolean(formik.touched.policy && formik.errors.policy) && (
+              <FormHelperText error>
+                {formik.errors.policy}
+              </FormHelperText>
+            )}
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Sign Up Now
+              </Button>
+            </Box>
+            <Typography
+              color="textSecondary"
+              variant="body2"
+            >
+              Have an account?
+              {' '}
+              <NextLink
+                href="/"
+                passHref
+              >
+                <Link
+                  variant="subtitle2"
+                  underline="hover"
+                >
+                  Sign In
+                </Link>
+              </NextLink>
+            </Typography>
+          </form>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.user.profile
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  save_user_data: (data) =>
+    dispatch({ type: Types.LOGIN, payload: data }),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
